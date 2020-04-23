5275a3dca71e430a5c0bdaa5ba411086992be15d
thejameswalsh <35312608+thejameswalsh@users.noreply.github.com>
2020-04-23 17:02:19 +1000

    adding matrix trucation to volumes
    this gets rid of all the extra plots that returned 000

---
+++ checkingVolumes.m
@@ -0,0 +1,34 @@
+%% size of cloud
+
+plot3(pointCloud(:,1),pointCloud(:,2),pointCloud(:,3),'r.');
+
+%% finding max reach
+
+'first method'
+load('Pcloud');
+
+[k, Max_Vol] = convhull(pointCloud);
+
+Max_Vol
+
+%%
+'second method'
+load('PcloudReduced');
+
+[k, Max_Vol] = convhull(pointCloud);
+
+Max_Vol
+%%
+'third method'
+[pCloudSize, c] = size(pointCloud);
+
+for i = 1 : pCloudSize
+    if pointCloud(i,:) == [0,0,0]
+        pointCloud = pointCloud(1:i,:);
+        break
+    end
+end
+
+[k, Max_Vol] = convhull(pointCloud);
+
+Max_Vol
